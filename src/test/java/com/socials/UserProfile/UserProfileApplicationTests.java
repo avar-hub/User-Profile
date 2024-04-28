97b3f44211347d8b1a591d0cb44cf8204844780d
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 <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>
2024-04-29 00:26:40 +0530

    trial

--- src/test/java/com/socials/UserProfile/UserProfileApplicationTests.java
+++ src/test/java/com/socials/UserProfile/UserProfileApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.socials.UserProfile;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class UserProfileApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.socials.UserProfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
